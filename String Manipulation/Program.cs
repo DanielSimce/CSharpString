6513eb901d24569adf286d428248b244ed729a01
Daniel <daniel.simonovski24@gmail.com>
2021-03-17 16:14:11 +0100

    start

---
+++ String Manipulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myString = "Daniel Simonovski";
+            Console.WriteLine(myString.ToLower());
+            Console.WriteLine(myString.ToUpper());
+    
+            Console.WriteLine(myString.PadLeft(100));
+            Console.WriteLine(myString.PadRight(100));
+
+            char myChar = myString[1];
+            Console.WriteLine(myChar);
+            char[] myChars = myString.ToCharArray();
+            foreach (char character in myString)
+            {
+                Console.Write($"{ character} ");
+            }
+            Console.WriteLine($"\nYou typed {myString.Length} characters. ");
+
+
+            Console.WriteLine(myChars);
+            Console.WriteLine(myChar);
+            Console.ReadLine();
+        }
+    }
+}
